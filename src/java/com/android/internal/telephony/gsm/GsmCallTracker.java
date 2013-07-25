951c307608c10361a8894d11afcd145a9ac167a7
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-07-25 23:09:05 +0100

    Update all CM radio code for the current API
    Change-Id: Ic29c2e06d48fdf8a7f0ea6d859c5d30e046a8940
    
    Conflicts:
    	src/java/com/android/internal/telephony/gsm/GSMPhone.java

--- src/java/com/android/internal/telephony/gsm/GsmCallTracker.java
+++ src/java/com/android/internal/telephony/gsm/GsmCallTracker.java
@@ -83,7 +83,7 @@ public final class GsmCallTracker extends CallTracker {
     boolean mHangupPendingMO;
 
     //Used to re-request the list of current calls
-    boolean slowModem = (SystemProperties.getInt("ro.telephony.slowModem",0) != 0);
+    boolean mSlowModem = (SystemProperties.getInt("ro.telephony.slowModem",0) != 0);
     GSMPhone mPhone;
 
     boolean mDesiredMute = false;    // false = mute off
@@ -454,10 +454,10 @@ public final class GsmCallTracker extends CallTracker {
         boolean needsPollDelay = false;
         boolean unknownConnectionAppeared = false;
 
-        if (slowModem) {
-            if (polledCalls.size() == 0 && !hangupPendingMO && pendingMO != null) {
-                lastRelevantPoll = obtainMessage(EVENT_POLL_CALLS_RESULT);
-                cm.getCurrentCalls(lastRelevantPoll);
+        if (mSlowModem) {
+            if (polledCalls.size() == 0 && !mHangupPendingMO && mPendingMO != null) {
+                mLastRelevantPoll = obtainMessage(EVENT_POLL_CALLS_RESULT);
+                mCi.getCurrentCalls(mLastRelevantPoll);
                 return;
             }
         }
@@ -789,7 +789,7 @@ public final class GsmCallTracker extends CallTracker {
                     "does not belong to GsmCallTracker " + this);
         }
 
-        hangupPendingMO = true;
+        mHangupPendingMO = true;
         call.onHangupLocal();
         mPhone.notifyPreciseCallStateChanged();
     }
